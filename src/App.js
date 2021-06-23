b16577b43d7aef9a16320eb7b3b8327688c2fdab
lbadocha87 <63849341+lbadocha87@users.noreply.github.com>
2021-06-23 22:21:44 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [pagination, setPagination] = useState([]);
+  const [page, setPage] = useState(1);
+
+
+  const getData = (pageNo) => {
+    axios.get('https://swapi.dev/api/planets/?page=' + pageNo)
+      .then(res => {
+        setData(res.data.results)
+      });
+  }
+
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/planets/')
+      .then(res => {
+        let links = [];
+        for(let i = 1; i<=Math.ceil(res.data.count/10); i++) {
+          links.push(<span key={i} onClick={()=>setPage(i)}>{i}</span>);
+        }
+        setPagination(links)
+      });
+
+    getData(page);
+  }, [page])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h2>Page {page}</h2>
+      {data.map((planet, index) => {
+        return (
+          <div key={index}>
+            <h3>{planet.name}</h3>
+          </div>
+        )
+      })}
+
+      <div className="pagination">
+        {pagination}
+      </div>
     </div>
   );
 }
